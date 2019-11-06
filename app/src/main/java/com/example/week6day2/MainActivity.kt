336009646610245c69cc5e2e01213bf5bede62c1
Chris Johnson <cljohnson4343@gmail.com>
2019-11-06 09:29:40 -0600

    Add button that starts imageview animations.

--- app/src/main/java/com/example/week6day2/MainActivity.kt
+++ app/src/main/java/com/example/week6day2/MainActivity.kt
@@ -1,12 +1,94 @@
 package com.example.week6day2
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AnimationSet
+import android.view.animation.LinearInterpolator
+import android.widget.Button
+import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fireBtn: Button
+    private lateinit var container: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        container = findViewById(R.id.container) as ViewGroup
+
+        fireBtn = findViewById(R.id.btn_fire) as Button
+        fireBtn.setOnClickListener { _ -> fire() }
+
+    }
+
+    fun fire() {
+        val bug = AppCompatImageView(this).apply {
+            setImageResource(R.drawable.ic_bug_droid)
+            setBackgroundColor(resources.getColor(R.color.black))
+            layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+        }
+        ImageViewCompat.setImageTintList(
+            bug,
+            ContextCompat.getColorStateList(this, R.color.bug_green)
+        )
+
+        var bugH = bug.height.toFloat()
+        var bugW = bug.width.toFloat()
+
+        val scaleW = Math.random().toFloat() * 1.9f + 0.1f
+        val scaleH = scaleW
+        bug.scaleX = scaleW
+        bug.scaleY = scaleH
+
+        bugH *= scaleH
+        bugW *= scaleW
+
+        val containerH = container.height
+        val containerW = container.width
+
+        bug.translationX = (Math.random() * containerW).toFloat() - bugW / 2.0f
+        bug.translationY = containerH.toFloat()
+
+
+        container.addView(bug)
+
+        val launchH = (Math.random() * containerH).toFloat() + bugH
+        val launcher =
+            ObjectAnimator.ofFloat(bug, View.TRANSLATION_Y, containerH.toFloat(), launchH).apply {
+                interpolator = AccelerateDecelerateInterpolator()
+                repeatCount = 1
+                repeatMode = ObjectAnimator.REVERSE
+            }
+
+        val rotator = ObjectAnimator.ofFloat(bug, View.ROTATION, (Math.random() * 1080).toFloat())
+
+        val set = AnimatorSet().apply {
+            playTogether(rotator, launcher)
+            duration = (Math.random() * 2500).toLong() + 1000
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    container.removeView(bug)
+                }
+            })
+        }
+        set.start()
+
     }
 }
